6f0a48ea2567b1222dcf3589a131ee97f243118d
Clarkmeister <arthur.clark@oit.edu>
2016-04-06 15:06:49 -0700

    Commit 3: Stadium Added, Card Created

---
+++ PokeDeck-Unity/Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+    public int m_xAxis;
+    public int m_yAxis;
+    public int m_zAxis;
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(m_xAxis, m_yAxis, m_zAxis);
+	}
+}
