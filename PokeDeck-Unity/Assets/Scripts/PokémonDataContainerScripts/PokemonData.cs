b869ab3501587a38eb1c3c449382eefaa7caad61
Ignistorm <Ryan.Bell@oit.edu>
2016-04-14 17:12:59 -0700

    Restructed unity directories and layout

---
+++ PokeDeck-Unity/Assets/Scripts/PokémonDataContainerScripts/PokemonData.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PokemonData : MonoBehaviour
+{
+	public enum EVOLUTION_STAGE {FIRST_EVOLUTION, SECOND_EVOLUTION, FINAL_EVOLUTION}
+
+	public enum ENERGY_TYPE {GRASS, WATER, FIRE, FIGHTING, LIGHTNING, PSYCHIC, COLORLESS}
+
+	
+
+	string m_name { get; set; }
+	int m_HP { get; set; }
+	EVOLUTION_STAGE m_evolutionStage { get; set; }
+	string m_nextEvolution { get; set; }
+	string m_previousEvolution { get; set; }
+	ENERGY_TYPE m_energyType { get; set; }
+	ENERGY_TYPE m_weakness {get; set; }
+	int m_resistanceAddend { get; set; } 
+
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
