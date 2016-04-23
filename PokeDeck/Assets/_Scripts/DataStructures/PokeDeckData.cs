3b36e0a7ebb7cf29fa042f89f8ddcffaeb311862
Clarkmeister <arthur.clark@oit.edu>
2016-04-23 12:25:43 -0700

    Settings Menu Now Controls Actual Master Volume and Music Volume

--- PokeDeck/Assets/_Scripts/DataStructures/PokeDeckData.cs
+++ PokeDeck/Assets/_Scripts/DataStructures/PokeDeckData.cs
@@ -1,23 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class PokeDeckData : MonoBehaviour {
+public class PokeDeckData : MonoBehaviour
+{
+	public Trainer PlayerOne { get; set; }
+	public Trainer PlayerTwo { get; set; }
+	public Settings GameSettings { get; set; }
+    public AudioSource BackgroundMusic;
 
-	Trainer m_PlayerOne;
-	Trainer m_PlayerTwo;
-	Settings m_GameSettings;
-	// Use this for initialization
+
 	void Start ()
 	{
 		DontDestroyOnLoad(this);
-		m_PlayerOne = Trainer.CreateInstance<Trainer>();
-		m_PlayerTwo = Trainer.CreateInstance<Trainer>();
-		m_GameSettings = Settings.CreateInstance<Settings>();
-		m_GameSettings.LoadSettings();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+		PlayerOne = Trainer.CreateInstance<Trainer>();
+		PlayerTwo = Trainer.CreateInstance<Trainer>();
+		GameSettings = Settings.CreateInstance<Settings>();
+		GameSettings.LoadSettings();
+        AudioListener.volume = GameSettings.MasterVolume;
+    }
+
+    IEnumerator Example()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(5);
+        print(Time.time);
+    }
+    // Update is called once per frame
+    void Update ()
 	{
 	
 	}
---
+++ PokeDeck/Assets/_Scripts/Settings/SettingsScreenScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SettingsScreenScript : MonoBehaviour {
+
+    PokeDeckData _GameData;
+    Slider _MasterVolumeSlider;
+    Slider _MusicVolumeSlider;
+
+	// Use this for initialization
+	void Start () {
+        GameObject data = GameObject.Find("PokeDeckGameData");
+        _GameData = data.GetComponent<PokeDeckData>();
+        data = GameObject.Find("MasterVolumeSlider");
+        _MasterVolumeSlider = data.GetComponent<Slider>();
+        data = GameObject.Find("MusicVolumeSlider");
+        _MusicVolumeSlider = data.GetComponent<Slider>();
+        _MasterVolumeSlider.value = _GameData.GameSettings.MasterVolume;
+        _MusicVolumeSlider.value = _GameData.GameSettings.MusicVolume;
+
+    }
+	
+    public void MasterVolumeUpdater()
+    {
+        _GameData.GameSettings.MasterVolume = _MasterVolumeSlider.value;
+        AudioListener.volume = _GameData.GameSettings.MasterVolume;
+    }
+
+    public void MusicVolumeUpdater()
+    {
+        _GameData.GameSettings.MusicVolume = _MusicVolumeSlider.value;
+        _GameData.BackgroundMusic.volume = _GameData.GameSettings.MusicVolume;
+    }
+
+}
