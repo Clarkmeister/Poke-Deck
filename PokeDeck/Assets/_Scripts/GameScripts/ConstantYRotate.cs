e4edcb7e7dd29c86b33e8b98ec5c9e9003da9690
Clarkmeister <arthur.clark@oit.edu>
2016-04-24 16:44:43 -0700

    PlayBoard Screen Created with mock-up Cards in view, Play button on main menu will take player to PlayBoard.

---
+++ PokeDeck/Assets/_Scripts/GameScripts/ConstantYRotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantYRotate : MonoBehaviour {
+
+    public GameObject thisObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisObject.transform.Rotate(.5f, .0f, .0f);
+    }
+}
